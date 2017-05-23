5cf3816709e622751732b6e3b1ecc7522f836490
Sri <srirangan@gmail.com>
2017-05-23 12:38:17 +0200

    setup - remove seed

--- app/setup.js
+++ app/setup.js
@@ -11,21 +11,18 @@ const labels = {
     path_to_geth: 'Path to geth',
     password: 'Password',
     password_confirm: 'Confirm password',
-    seed: 'Wallet seed',
-    seed_text: 'Generated wallet seed (keep it safe)',
 };
 
-const get_setup = () => JSON.parse(fs.readFileSync(path));
+const get_setup = () => {
+    if (!fs.existsSync(path)) return {};
+    else return JSON.parse(fs.readFileSync(path));
+};
 
 const geth_path_is_invalid = path => {
-    let invalid = false;
-    const geth_path_does_not_exist = !path || path.length === 0;
-    const geth_does_not_exist = !geth_path_does_not_exist && !fs.existsSync(path);
-    const geth_path_is_a_dir = !geth_does_not_exist && fs.statSync(path).isDirectory();
-    if (geth_path_does_not_exist) invalid = true;
-    else if (geth_path_is_a_dir) invalid = true;
-    else if (geth_does_not_exist) invalid = true;
-    return invalid;
+    if (!path || path.length === 0) return true;
+    else if (!fs.existsSync(path)) return true;
+    else if (fs.statSync(path).isDirectory()) return true;
+    return false;
 };
 
 const invalid = () => {
@@ -34,14 +31,15 @@ const invalid = () => {
     if (setup_file_does_not_exist) invalid = true;
     else {
         const setup = get_setup();
-        invalid = geth_path_is_invalid(setup.geth) || !setup.password || !setup.seed || !setup.keystore;
+        invalid = geth_path_is_invalid(setup.geth) || !setup.password || !setup.keystore;
     }
     return invalid;
 };
 
+const save = data => fs.writeFileSync(path, JSON.stringify(data, null, 2));
+
 const render = () => {
-    let setup = {};
-    if (fs.existsSync(path)) setup = get_setup();
+    let setup = get_setup();
     const title = dom_js.create_element('h1.title', null, [labels.title]);
     const button = dom_js.create_element(
         'button.button',
@@ -51,17 +49,12 @@ const render = () => {
     let input_geth_path;
     let input_password;
     let input_password_confirm;
-    let text_seed;
-    let input_seed;
     const password_is_invalid = () => {
         const password = input_password.value;
         const confirm = input_password_confirm.value;
         return password.length === 0 || password !== confirm;
     };
     const verify = event => {
-        if (!password_is_invalid() && !get_setup().keystore) {
-            input_seed.value = lightwallet.keystore.generateRandomSeed(input_password.value);
-        }
         button.disabled = geth_path_is_invalid(input_geth_path.value) || password_is_invalid();
         if (!button.disabled && event.keyCode === dom_js.key_codes.enter) button.click();
     };
@@ -73,46 +66,33 @@ const render = () => {
     );
     input_password = dom_js.create_element(
         'input.input',
-        {placeholder: labels.password, type: 'password', value: setup.password || '', readonly: setup.password},
+        {placeholder: labels.password, type: 'password', value: setup.password || ''},
         null,
         {keyup: verify}
     );
     input_password_confirm = dom_js.create_element(
         'input.input',
-        {placeholder: labels.password_confirm, type: 'password', value: setup.password || '', readonly: setup.password},
+        {placeholder: labels.password_confirm, type: 'password', value: setup.password || ''},
         null,
         {keyup: verify}
     );
-    text_seed = dom_js.create_element('div.text', null, [labels.seed_text]);
-    input_seed = dom_js.create_element(
-        'input.input seed',
-        {placeholder: labels.seed, type: 'text', readonly: true, value: setup.seed || ''},
-        null,
-        {keyup: verify}
-    );
-    const save = () => {
-        lightwallet.keystore.deriveKeyFromPassword(input_password.value, (err, derived_key) => {
-            const keystore = new lightwallet.keystore(input_seed.value, derived_key);
-            const data = {
+    const on_save_click = () => {
+        lightwallet.keystore.createVault({password: input_password.value}, (err, keystore) => {
+            save({
                 geth: input_geth_path.value,
                 password: input_password.value,
-                seed: get_setup().seed || input_seed.value,
                 keystore: get_setup().keystore || keystore.serialize(),
                 accounts: get_setup().accounts || {},
-            };
-            fs.writeFileSync(path, JSON.stringify(data, null, 2));
-            dom_js.empty_element(root);
+            });
             events.trigger('start');
         });
     };
-    dom_js.add_event_listeners(button, {click: save});
+    dom_js.add_event_listeners(button, {click: on_save_click});
     const view = dom_js.create_element('div.setup', null, [
         title,
         input_geth_path,
         input_password,
         input_password_confirm,
-        text_seed,
-        input_seed,
         button
     ]);
     const root = document.querySelector('#root');
@@ -120,4 +100,4 @@ const render = () => {
     dom_js.append_child(root, view);
 };
 
-module.exports = {invalid, render, get_setup};
+module.exports = {invalid, render, get_setup, save};
