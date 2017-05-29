e58efb02277f28e4e9f5a131c70fafa614d15174
Sri <srirangan@gmail.com>
2017-05-29 16:58:47 +0200

    tabs switcher

--- src/react/app.js
+++ src/react/app.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classnames from 'classnames';
 
-const root = document.querySelector('#root');
+// ------ Tabs ------
 
-class App extends React.Component {
+class Tabs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selected: 0 };
+    }
+
     render() {
-        return (<h1>Hello World!</h1>);
+        const buttons = [];
+        let content = null;
+        this.props.children.forEach((child, index) => {
+            const on_click = () => this.setState({selected: index});
+            buttons.push(
+                <span className={classnames({Button: true, Selected: this.state.selected === index})}
+                      onClick={on_click}>{child.label}</span>
+            );
+        });
+        return <div className='Tabs'>
+            <div className='Buttons'>{buttons}</div>
+            <div className='Content'>{this.props.children[this.state.selected].content}</div>
+        </div>;
     }
 }
 
-ReactDOM.render(<App/>, root);
+// ------ Wallet ------
+
+class Wallet extends React.Component {
+    render() {
+        return <h1>Wallet</h1>;
+    }
+}
+
+// ------ CoinAnalysis ------
+
+class CoinAnalysis extends React.Component {
+    render() {
+        return <h1>CoinAnalysis</h1>;
+    }
+}
+
+// ------ App ------
+
+class App extends React.Component {
+    render() {
+        return <div className='App'>
+            <Tabs children={[
+                { label: 'Wallet', content: <Wallet/> },
+                { label: 'Coin Analysis', content: <CoinAnalysis/> },
+            ]}/>
+        </div>;
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
